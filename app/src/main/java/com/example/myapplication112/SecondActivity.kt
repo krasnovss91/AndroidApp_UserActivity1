ef5f4f20bfbf505617a95766ffd5badabf3fdc75
krasnovss91 <krasnovstanislav91@gmail.com>
2022-02-08 12:48:39 +0400

    activities_12

--- app/src/main/java/com/example/myapplication112/SecondActivity.kt
+++ app/src/main/java/com/example/myapplication112/SecondActivity.kt
@@ -1,9 +1,11 @@
 package com.example.myapplication112
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import androidx.appcompat.widget.AppCompatButton
 
 class SecondActivity : AppCompatActivity() {
@@ -12,16 +14,21 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName3)
+        val intent = Intent(this, SecondActivity::class.java)
+
         // val arguments = intent.extras  ловим данные из предыдущей activity
         // val data = arguments?.get("result").toString()
 
         val button = findViewById<AppCompatButton>(R.id.button3)
 
-        button.setOnClickListener(object : View.OnClickListener {//здесь возврат в предыдущую activity
-        override fun onClick(v: View?) {
-            //  intent.putExtra("result", editText_1.text.toString() + editText_2.text.toString())
-            //   startActivity(intent)
-        }
+        button.setOnClickListener(object : View.OnClickListener {
+            //здесь возврат в предыдущую activity
+            override fun onClick(v: View?) {
+                intent.putExtra("data", editText.text.toString())
+                startActivity(intent)
+
+            }
         })
     }
 }
